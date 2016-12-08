69be56862cbbf7b2f66b393d49ec7a4f6743eba7
John Loftus <johnloftus026@gmail.com>
2016-12-08 14:16:19 -0800

    Add UI

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,6 +4,7 @@ def encrypt(str)
   i = 0
   nstr = ""
   while i < str.length
+    #case when there are spaces
     if str[i] != " "
       aix = $alph.index(str[i])
       #case for letter z and wrap to a
@@ -24,6 +25,7 @@ def decrypt(str)
   i = 0
   nstr = ""
   while i < str.length
+    #case when there are spaces
     if str[i] != " "
       aix = $alph.index(str[i])
       #case for letter a and wrap to z
@@ -39,6 +41,28 @@ def decrypt(str)
   return nstr
 end
 
-puts encrypt("hello worldz")
+# puts encrypt("hello worldz")
 
-puts decrypt(encrypt("hello worldz"))
+# puts decrypt(encrypt("hello worldz"))
+
+#Our drive code
+
+#ask user to decrypt or encrypt a password
+puts "Would you like to encrypt or decrypt?"
+op = gets.chomp
+while !(op == "encrypt" || op == "decrypt")
+  puts "Please decide on 'encrypt' or 'decrypt'."
+  op = gets.chomp
+end
+
+#ask user for password
+puts "What is the password you would like to use?"
+input = gets.chomp
+
+
+#prints result to user
+if op == "encrypt"
+  puts "Encrypted: " + input + " => " + encrypt(input)
+else
+  puts "Decrypted: " + input + " => " + decrypt(input)
+end
