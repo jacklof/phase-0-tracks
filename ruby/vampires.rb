a81156b8a4cbba9c8ec3948f844fe120f54ae6c4
John Loftus <johnloftus026@gmail.com>
2016-12-08 19:04:30 -0800

    Add data collection

---
+++ ruby/vampires.rb
@@ -0,0 +1,30 @@
+puts "What is your name?"
+name = gets.chomp.capitalize
+
+puts "How old are you?"
+age = gets.chomp.to_i
+while age <= -1
+  puts "You must enter a positive integer"
+  age = gets.chomp.to_i
+end
+
+puts "What year were you born?"
+year = gets.chomp.to_i
+while year == 0
+  puts "Please enter a valid year"
+  year = gets.chomp.to_i
+end
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+bread = gets.chomp.downcase
+while !(bread == "y" || bread == "n")
+  puts "Please enter Y or N"
+  bread = gets.chomp.downcase
+end
+
+puts "Would you like to enroll in the company's health insurance?"
+insur = gets.chomp.downcase
+while !(insur == "y" || insur == "n")
+  puts "Please enter Y or N"
+  insur = gets.chomp.downcase
+end
