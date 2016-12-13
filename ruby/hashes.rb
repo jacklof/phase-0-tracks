0f7780c3313a500d31ecb4a8631c62b4263cc8f4
Jack Loftus <johnloftus026@gmail.com>
2016-12-12 17:53:24 -0800

    Add pseudocode program

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+# Define a method that asks the user to input name, age, number of children, decor theme, and wallpaper preferences, as well as if the client wants to paint the outside of the house (if this is the case, also ask for the color).
+# After asking for the name, capitalize it appropriately.
+# After asking for the age, attempt to parse it as number.
+# If it is not a number, or is not a whole, positive number, ask again.
+# Do the same for number of children.
+# After asking for the decor theme, ensure that it is one of several provided options.
+# If it is not, ask again until it is.
+# Do the same for wallpaper preferences.
+# After asking if the client wants to paint, attempt to parse the Y/N answer into boolean, asking over and over again until yes or no is inputted (also taking true/false).
+# If the input is yes/true, ask for a color to paint.
+# Otherwise, set the color option to nil.
+# Continue asking the user for a color until the response is one of several provided options.
+# Print to screen each value, preceeded by some phrase representative of the value's purpose.
+# Ask which key the user wants to edit, or 'none' for no edit.
+# Parse the input as a hash symbol, assuming the user has inputted an existing key-equivalent string.
+# Replace the key's value with the new inputted value, parsed correctly according to above constraints.
+# Print to screen all values again, with corresponding labels.
+# Exit.
