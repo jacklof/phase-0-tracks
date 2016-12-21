06cc3ce04658b4c162d2882f89a4df82852d743f
Jack Loftus <johnloftus026@gmail.com>
2016-12-21 14:23:17 -0800

    Add methods and driver code to execute them

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(n)
+    n.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def sit
+    puts "*sits*"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 end
 
+#driver
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+fido.dog_years(4)
+fido.sit
