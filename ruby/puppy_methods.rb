edd3a6328a81dd59ecc8faaa4272c619eb597329
Jack Loftus <johnloftus026@gmail.com>
2016-12-21 14:35:17 -0800

    Add another class definition and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,8 @@
+# 6.2 Pairing Instance methods
+# John Loftus
+# Joseph Huang
+# Time: 30m
+
 class Puppy
 
   def fetch(toy)
@@ -27,10 +32,45 @@ class Puppy
   end
 end
 
-#driver
-fido = Puppy.new
-fido.fetch("ball")
-fido.speak(3)
-fido.roll_over
-fido.dog_years(4)
-fido.sit
+class Cook
+
+  def initialize
+    puts "The cook is preparing..."
+  end
+
+  def ignite_stove
+    puts "Lit the ignition"
+  end
+
+  def pan_size(size)
+    puts "The cook chose pan-size: #{size}."
+  end
+
+end
+
+#
+# Driver code for class Puppy
+#
+# fido = Puppy.new
+# fido.fetch("ball")
+# fido.speak(3)
+# fido.roll_over
+# fido.dog_years(4)
+# fido.sit
+
+#
+# Driver code for class Cook
+#
+
+# Stores all of the instances in a data structure
+cook_array = []
+50.times do |index|
+  cook_array[index] = Cook.new
+end
+
+# Iterate over data structure cook using .each and call the instance methods
+cook_array.each do |cook|
+  puts "\nCook No. #{cook_array.index(cook) + 1}"
+  cook.ignite_stove
+  cook.pan_size(4)
+end
