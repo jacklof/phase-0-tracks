10dee48523f1d0fb8b6c8cb708c64c43f8e2846b
Jack Loftus <johnloftus026@gmail.com>
2016-12-31 17:40:46 -0800

    Add new mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,34 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words + "!!! :) :)"
+#   end
+# end
+#
+# puts Shout.yell_angrily("Grr I'm mad")
+# puts Shout.yelling_happily("Time for cake")
 
-  def self.yelling_happily(words)
-    words + "!!! :) :)"
+module Shout
+  def yell(words)
+    words + "!!!"
+  end
+  def shout(words)
+    words.upcase + "!!!"
   end
 end
 
-puts Shout.yell_angrily("Grr I'm mad")
-puts Shout.yelling_happily("Time for cake")
+class Human
+  include Shout
+end
+
+class Alien
+  include Shout
+end
+
+human = Human.new
+alien = Alien.new
+puts human.yell("I'm an angry person")
+puts alien.shout("Grrraabbaallioacnnn")
