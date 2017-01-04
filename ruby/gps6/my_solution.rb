d17200336c1b4b046598592e7e75371e6ac883a4
Jack Loftus <johnloftus026@gmail.com>
2017-01-04 15:28:03 -0800

    Add comments to each method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -10,12 +10,14 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  # Will populate each instance with the values grabbed from STATE_DATA
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # Runs private methods to print useful data
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
@@ -23,6 +25,7 @@ class VirusPredictor
 
   private
 
+  # Returns a number as a function of each instance's population densities and populations, interpolating it as a printable sentence
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +44,7 @@ class VirusPredictor
 
   end
 
+  # Returns a given speed based on population density
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
