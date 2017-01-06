0a63c702d411ec5524c73d1902881f832a75b3a9
Jack Loftus <johnloftus026@gmail.com>
2017-01-05 20:17:13 -0800

    Add alternate notation

--- js/data_structures.js
+++ js/data_structures.js
@@ -26,6 +26,10 @@ function Car(color, year, isRunning) {
 
  var car2 = new Car("blue", 2006, false);
  console.log(car2);
+ console.log(car2['color']);
+ console.log(car2['year']);
 
  var car3 = new Car("green", 2010, true);
  console.log(car3);
+ console.log(car3.color);
+ console.log(car3.year);
