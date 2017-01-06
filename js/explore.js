1cd3b38d5318abdd1f225ed3fd3a18bb7744df73
Jack Loftus <johnloftus026@gmail.com>
2017-01-05 18:44:43 -0800

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+/*
+
+Function: reverse(str)
+- Create an empty array
+- Iterate through the letters of str
+- For each index, assign the created array indexes to
+the difference of the length of str and the current
+index of the iteration
+
+*/
