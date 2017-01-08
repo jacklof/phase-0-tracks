51cf83b70c1eaa2f78dd82e210073a6d4487c7b7
Jack Loftus <johnloftus026@gmail.com>
2017-01-07 17:51:17 -0800

    Add pseudocode for longest phrase

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+/*
+Define a function that finds the longest string in an array of strings
+  Input: an array of strings
+  Steps:
+    - Create a string var to be returned that it is equal to the value of the of the given array's first index
+    - Iterate through a for loop, starting with an index value of 1 and ending with index of array's length - 1
+      - If the given array's value at the current index is longer than the return string, set the return string to that value
+      - Otherwise, move on to the next index
+  Output: the longest string of the given array
+*/
