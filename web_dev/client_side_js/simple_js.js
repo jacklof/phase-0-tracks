1ebd1ecc31a893eb97c362d72b4460b7ce40d910
Jack Loftus <johnloftus026@gmail.com>
2017-01-17 18:30:00 -0800

    Add DOM manipulation

--- web_dev/client_side_js/simple_js.js
+++ web_dev/client_side_js/simple_js.js
@@ -1,1 +1,15 @@
 console.log("script loaded");
+
+function addBorder(event) {
+  if (event.type == "mouseover") {
+    event.target.style.border = "1px solid red";
+  } else {
+    event.target.style.border = "";
+  }
+}
+
+var pic = document.getElementById("glider-image");
+pic.style.width = "256px";
+pic.style.height = "auto";
+pic.addEventListener("mouseover", addBorder);
+pic.addEventListener("mouseout", addBorder);
