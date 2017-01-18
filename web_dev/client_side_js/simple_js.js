02b802a3812917c918636774262b7f6c3ddb1d44
Jack Loftus <johnloftus026@gmail.com>
2017-01-17 18:47:27 -0800

    Add functionality to add new elements

--- web_dev/client_side_js/simple_js.js
+++ web_dev/client_side_js/simple_js.js
@@ -3,9 +3,19 @@ console.log("script loaded");
 function addBorder(event) {
   if (event.type == "mouseover") {
     event.target.style.border = "1px solid red";
-  } else {
+  } else if (event.type == "mouseout") {
     event.target.style.border = "";
   }
+}
+
+function addText() {
+  var paragraph = document.createElement("p");
+
+  var text = document.createTextNode("This is some new text!");
+  paragraph.appendChild(text);
+
+  var div = document.getElementById("new-content-div");
+  div.appendChild(paragraph);
 }
 
 var pic = document.getElementById("glider-image");
@@ -13,3 +23,6 @@ pic.style.width = "256px";
 pic.style.height = "auto";
 pic.addEventListener("mouseover", addBorder);
 pic.addEventListener("mouseout", addBorder);
+
+var button = document.getElementById("create-list");
+button.addEventListener("click", addText);
