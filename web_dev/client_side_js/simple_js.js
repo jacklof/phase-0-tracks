e54ff6c0bbdcf46a677dc86c4d1ec53f042667de
Jack Loftus <johnloftus026@gmail.com>
2017-01-17 19:22:00 -0800

    Add functionality to change elements of a class

--- web_dev/client_side_js/simple_js.js
+++ web_dev/client_side_js/simple_js.js
@@ -1,6 +1,8 @@
 console.log("script loaded");
 
 function addBorder(event) {
+  console.log(event);
+
   if (event.type == "mouseover") {
     event.target.style.border = "1px solid red";
   } else if (event.type == "mouseout") {
@@ -8,7 +10,9 @@ function addBorder(event) {
   }
 }
 
-function addText() {
+function addText(event) {
+  console.log(event);
+
   var paragraph = document.createElement("p");
 
   var text = document.createTextNode("This is some new text!");
@@ -16,6 +20,13 @@ function addText() {
 
   var div = document.getElementById("new-content-div");
   div.appendChild(paragraph);
+}
+
+function changeClass(event) {
+  console.log(event);
+
+  var elements = document.getElementsByClassName("sample-class");
+  console.log(elements);
 }
 
 var pic = document.getElementById("glider-image");
@@ -26,3 +37,6 @@ pic.addEventListener("mouseout", addBorder);
 
 var button = document.getElementById("create-list");
 button.addEventListener("click", addText);
+
+var button2 = document.getElementById("class-alteration");
+button2.addEventListener("click", changeClass);
